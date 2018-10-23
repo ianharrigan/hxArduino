3811af85c82d500df1a1ada560e038a5cafb69e7
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-10-24 01:53:39 +0200

    code documentation

--- src/Arduino.hx
+++ src/Arduino.hx
@@ -2,8 +2,10 @@ package;
 
 @:include("Arduino.h")
 extern class Arduino {
+    // https://www.arduino.cc/reference/en/#functions
+
     public static inline var LED_BUILTIN:Int = untyped 'LED_BUILTIN';
-    
+
     public static  inline var A0:Int = untyped 'A0';
     public static  inline var A1:Int = untyped 'A1';
     public static  inline var A2:Int = untyped 'A2';
@@ -15,14 +17,26 @@ extern class Arduino {
 
     public static inline var INPUT:Int = untyped 'INPUT';
     public static inline var OUTPUT:Int = untyped 'OUTPUT';
-    
+
     public static inline var HIGH:Int = untyped 'HIGH';
     public static inline var LOW:Int = untyped 'LOW';
-    
+
+    // Digital I/O
     public static function pinMode(pin:Int, mode:Int):Void;
     public static function digitalWrite(pin:Int, value:Int):Void;
     public static function digitalRead(pin:Int):Int;
+
+    // Analog I/O
     public static function analogRead(pin:Int):Int;
+    // analogReference()
+    // analogWrite()
+
+    // Time
     public static function delay(amount:Int):Void;
+    // delayMicroseconds()
+    // micros()
+    // millis()
+
+
     public static function map(value:Int, fromLow:Int, fromHigh:Int, toLow:Int, toHigh:Int):Int;
 }
