a2a97d160c5564fe5e6dca2c41a1c38632bbd60b
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-10-24 02:34:12 +0200

    small update with Serial

--- examples/01-blink/src/Main.hx
+++ examples/01-blink/src/Main.hx
@@ -3,18 +3,23 @@ package;
 class Main {
     public function setup() {
         Arduino.pinMode(Arduino.LED_BUILTIN, Arduino.OUTPUT);
+        Serial.begin(115200);
     }
-    
+
     public function loop() {
         Arduino.digitalWrite(Arduino.LED_BUILTIN, Arduino.HIGH);
-        
+
+        Serial.println('high');
+
         Arduino.delay(1000);
-        
+
         Arduino.digitalWrite(Arduino.LED_BUILTIN, Arduino.LOW);
-        
+
+        Serial.println('low');
+
         Arduino.delay(1000);
     }
-    
+
     static function main() {
     }
 }
