01652f3527dd2a2e398f16acbb80cd86735e1585
Ian Harrigan <ianharrigan@hotmail.com>
2020-12-06 18:34:01 +0100

    fix linked list copy constructer

--- lib/LinkedList.h
+++ lib/LinkedList.h
@@ -1,12 +1,12 @@
 /*
-	LinkedList.h - V1.1 - Generic LinkedList implementation
-	Works better with FIFO, because LIFO will need to
-	search the entire List to find the last one;
+  LinkedList.h - V1.1 - Generic LinkedList implementation
+  Works better with FIFO, because LIFO will need to
+  search the entire List to find the last one;
 
-	For instructions, go to https://github.com/ivanseidel/LinkedList
+  For instructions, go to https://github.com/ivanseidel/LinkedList
 
-	Created by Ivan Seidel Gomes, March, 2013.
-	Released into the public domain.
+  Created by Ivan Seidel Gomes, March, 2013.
+  Released into the public domain.
 */
 
 
@@ -23,12 +23,12 @@ struct ListNode
 };
 
 template <typename T>
-class LinkedList{
+class LinkedList {
 
 protected:
 	int _size;
 	ListNode<T> *root;
-	ListNode<T>	*last;
+	ListNode<T> *last;
 
 	// Helps "get" method, by saving last position
 	ListNode<T> *lastNodeGot;
@@ -39,77 +39,85 @@ protected:
 
 	ListNode<T>* getNode(int index);
 
-	ListNode<T>* findEndOfSortedString(ListNode<T> *p, int (*cmp)(T &, T &));
+	ListNode<T>* findEndOfSortedString(ListNode<T> *p, int(*cmp)(T &, T &));
 
 public:
 	// Copy constructor 
-	LinkedList(const ListNode<T> &copy) {
-		_size = copy._size;
-		root = copy.root;
-		last = copy.last;
+	LinkedList(const LinkedList<T> &copy) {
+		LinkedList<T> &a = const_cast<LinkedList<T>&>(copy);
+		for (int i = 0; i < a.size(); i++) {
+			add(a.get(i));
+		}
+	}
+
+	void operator = (const LinkedList<T> &rhs) {
+		LinkedList<T> &a = const_cast<LinkedList<T>&>(rhs);
+		for (int i = 0; i < a.size(); i++) {
+			add(a.get(i));
+		}
 	}
 
 	LinkedList();
 	~LinkedList();
 
 	/*
-		Returns current size of LinkedList
+	  Returns current size of LinkedList
 	*/
 	virtual int size();
 	/*
-		Adds a T object in the specified index;
-		Unlink and link the LinkedList correcly;
-		Increment _size
+	  Adds a T object in the specified index;
+	  Unlink and link the LinkedList correcly;
+	  Increment _size
 	*/
 	virtual bool add(int index, T);
 	/*
-		Adds a T object in the end of the LinkedList;
-		Increment _size;
+	  Adds a T object in the end of the LinkedList;
+	  Increment _size;
 	*/
 	virtual bool add(T);
 	/*
-		Adds a T object in the start of the LinkedList;
-		Increment _size;
+	  Adds a T object in the start of the LinkedList;
+	  Increment _size;
 	*/
 	virtual bool unshift(T);
 	/*
-		Set the object at index, with T;
-		Increment _size;
+	  Set the object at index, with T;
+	  Increment _size;
 	*/
 	virtual bool set(int index, T);
 	/*
-		Remove object at index;
-		If index is not reachable, returns false;
-		else, decrement _size
+	  Remove object at index;
+	  If index is not reachable, returns false;
+	  else, decrement _size
 	*/
 	virtual T remove(int index);
 	/*
-		Remove last object;
+	  Remove last object;
 	*/
 	virtual T pop();
 	/*
-		Remove first object;
+	  Remove first object;
 	*/
 	virtual T shift();
 	/*
-		Get the index'th element on the list;
-		Return Element if accessible,
-		else, return false;
+	  Get the index'th element on the list;
+	  Return Element if accessible,
+	  else, return false;
 	*/
 	virtual T get(int index);
 
 	/*
-		Clear the entire array
+	  Clear the entire array
 	*/
 	virtual void clear();
 
 	/*
-		Sort the list, given a comparison function
+	  Sort the list, given a comparison function
 	*/
-	virtual void sort(int (*cmp)(T &, T &));
+	virtual void sort(int(*cmp)(T &, T &));
 
 	// added concat function (likely ineffecient - esp for large arrays - which probably wouldnt make sense on arduino anyway)
-	LinkedList<T> concat(LinkedList &a) {
+	LinkedList<T> concat(LinkedList<T> &a) {
 		LinkedList<T> out;
 		for (int i = 0; i < size(); i++) {
 			out.add(get(i));
@@ -117,6 +125,7 @@ public:
 		for (int i = 0; i < a.size(); i++) {
 			out.add(a.get(i));
 		}
+
 		return out;
 	}
 
@@ -126,9 +135,9 @@ public:
 template<typename T>
 LinkedList<T>::LinkedList()
 {
-	root=NULL;
-	last=NULL;
-	_size=0;
+	root = NULL;
+	last = NULL;
+	_size = 0;
 
 	lastNodeGot = root;
 	lastIndexGot = 0;
@@ -140,42 +149,42 @@ template<typename T>
 LinkedList<T>::~LinkedList()
 {
 	ListNode<T>* tmp;
-	while(root!=NULL)
+	while (root != NULL)
 	{
-		tmp=root;
-		root=root->next;
+		tmp = root;
+		root = root->next;
 		delete tmp;
 	}
 	last = NULL;
-	_size=0;
+	_size = 0;
 	isCached = false;
 }
 
 /*
-	Actualy "logic" coding
+  Actualy "logic" coding
 */
 
 template<typename T>
-ListNode<T>* LinkedList<T>::getNode(int index){
+ListNode<T>* LinkedList<T>::getNode(int index) {
 
 	int _pos = 0;
 	ListNode<T>* current = root;
 
 	// Check if the node trying to get is
 	// immediatly AFTER the previous got one
-	if(isCached && lastIndexGot <= index){
+	if (isCached && lastIndexGot <= index) {
 		_pos = lastIndexGot;
 		current = lastNodeGot;
 	}
 
-	while(_pos < index && current){
+	while (_pos < index && current) {
 		current = current->next;
 
 		_pos++;
 	}
 
 	// Check if the object index got is the same as the required
-	if(_pos == index){
+	if (_pos == index) {
 		isCached = true;
 		lastIndexGot = index;
 		lastNodeGot = current;
@@ -183,25 +192,25 @@ ListNode<T>* LinkedList<T>::getNode(int index){
 		return current;
 	}
 
-	return false;
+	return NULL;
 }
 
 template<typename T>
-int LinkedList<T>::size(){
+int LinkedList<T>::size() {
 	return _size;
 }
 
 template<typename T>
-bool LinkedList<T>::add(int index, T _t){
+bool LinkedList<T>::add(int index, T _t) {
 
-	if(index >= _size)
+	if (index >= _size)
 		return add(_t);
 
-	if(index == 0)
+	if (index == 0)
 		return unshift(_t);
 
 	ListNode<T> *tmp = new ListNode<T>(),
-				 *_prev = getNode(index-1);
+		*_prev = getNode(index - 1);
 	tmp->data = _t;
 	tmp->next = _prev->next;
 	_prev->next = tmp;
@@ -213,17 +222,18 @@ bool LinkedList<T>::add(int index, T _t){
 }
 
 template<typename T>
-bool LinkedList<T>::add(T _t){
+bool LinkedList<T>::add(T _t) {
 
 	ListNode<T> *tmp = new ListNode<T>();
 	tmp->data = _t;
 	tmp->next = NULL;
-	
-	if(root){
+
+	if (root) {
 		// Already have elements inserted
 		last->next = tmp;
 		last = tmp;
-	}else{
+	}
+	else {
 		// First element being inserted
 		root = tmp;
 		last = tmp;
@@ -236,26 +246,26 @@ bool LinkedList<T>::add(T _t){
 }
 
 template<typename T>
-bool LinkedList<T>::unshift(T _t){
+bool LinkedList<T>::unshift(T _t) {
 
-	if(_size == 0)
+	if (_size == 0)
 		return add(_t);
 
 	ListNode<T> *tmp = new ListNode<T>();
 	tmp->next = root;
 	tmp->data = _t;
 	root = tmp;
-	
+
 	_size++;
 	isCached = false;
-	
+
 	return true;
 }
 
 template<typename T>
-bool LinkedList<T>::set(int index, T _t){
+bool LinkedList<T>::set(int index, T _t) {
 	// Check if index position is in bounds
-	if(index < 0 || index >= _size)
+	if (index < 0 || index >= _size)
 		return false;
 
 	getNode(index)->data = _t;
@@ -263,13 +273,13 @@ bool LinkedList<T>::set(int index, T _t){
 }
 
 template<typename T>
-T LinkedList<T>::pop(){
-	if(_size <= 0)
+T LinkedList<T>::pop() {
+	if (_size <= 0)
 		return T();
-	
+
 	isCached = false;
 
-	if(_size >= 2){
+	if (_size >= 2) {
 		ListNode<T> *tmp = getNode(_size - 2);
 		T ret = tmp->next->data;
 		delete(tmp->next);
@@ -277,7 +287,8 @@ T LinkedList<T>::pop(){
 		last = tmp;
 		_size--;
 		return ret;
-	}else{
+	}
+	else {
 		// Only one element left on the list
 		T ret = root->data;
 		delete(root);
@@ -289,20 +300,21 @@ T LinkedList<T>::pop(){
 }
 
 template<typename T>
-T LinkedList<T>::shift(){
-	if(_size <= 0)
+T LinkedList<T>::shift() {
+	if (_size <= 0)
 		return T();
 
-	if(_size > 1){
+	if (_size > 1) {
 		ListNode<T> *_next = root->next;
 		T ret = root->data;
 		delete(root);
 		root = _next;
-		_size --;
+		_size--;
 		isCached = false;
 
 		return ret;
-	}else{
+	}
+	else {
 		// Only one left, then pop()
 		return pop();
 	}
@@ -310,16 +322,16 @@ T LinkedList<T>::shift(){
 }
 
 template<typename T>
-T LinkedList<T>::remove(int index){
+T LinkedList<T>::remove(int index) {
 	if (index < 0 || index >= _size)
 	{
 		return T();
 	}
 
-	if(index == 0)
+	if (index == 0)
 		return shift();
-	
-	if (index == _size-1)
+
+	if (index == _size - 1)
 	{
 		return pop();
 	}
@@ -336,32 +348,32 @@ T LinkedList<T>::remove(int index){
 
 
 template<typename T>
-T LinkedList<T>::get(int index){
+T LinkedList<T>::get(int index) {
 	ListNode<T> *tmp = getNode(index);
 
 	return (tmp ? tmp->data : T());
 }
 
 template<typename T>
-void LinkedList<T>::clear(){
-	while(size() > 0)
+void LinkedList<T>::clear() {
+	while (size() > 0)
 		shift();
 }
 
 template<typename T>
-void LinkedList<T>::sort(int (*cmp)(T &, T &)){
-	if(_size < 2) return; // trivial case;
+void LinkedList<T>::sort(int(*cmp)(T &, T &)) {
+	if (_size < 2) return; // trivial case;
 
-	for(;;) {	
+	for (;;) {
 
 		ListNode<T> **joinPoint = &root;
 
-		while(*joinPoint) {
+		while (*joinPoint) {
 			ListNode<T> *a = *joinPoint;
 			ListNode<T> *a_end = findEndOfSortedString(a, cmp);
-	
-			if(!a_end->next	) {
-				if(joinPoint == &root) {
+
+			if (!a_end->next) {
+				if (joinPoint == &root) {
 					last = a_end;
 					isCached = false;
 					return;
@@ -379,28 +391,28 @@ void LinkedList<T>::sort(int (*cmp)(T &, T &)){
 			a_end->next = NULL;
 			b_end->next = NULL;
 
-			while(a && b) {
-				if(cmp(a->data, b->data) <= 0) {
+			while (a && b) {
+				if (cmp(a->data, b->data) <= 0) {
 					*joinPoint = a;
 					joinPoint = &a->next;
-					a = a->next;	
+					a = a->next;
 				}
 				else {
 					*joinPoint = b;
 					joinPoint = &b->next;
-					b = b->next;	
+					b = b->next;
 				}
 			}
 
-			if(a) {
+			if (a) {
 				*joinPoint = a;
-				while(a->next) a = a->next;
+				while (a->next) a = a->next;
 				a->next = tail;
 				joinPoint = &a->next;
 			}
 			else {
 				*joinPoint = b;
-				while(b->next) b = b->next;
+				while (b->next) b = b->next;
 				b->next = tail;
 				joinPoint = &b->next;
 			}
@@ -409,11 +421,11 @@ void LinkedList<T>::sort(int (*cmp)(T &, T &)){
 }
 
 template<typename T>
-ListNode<T>* LinkedList<T>::findEndOfSortedString(ListNode<T> *p, int (*cmp)(T &, T &)) {
-	while(p->next && cmp(p->data, p->next->data) <= 0) {
+ListNode<T>* LinkedList<T>::findEndOfSortedString(ListNode<T> *p, int(*cmp)(T &, T &)) {
+	while (p->next && cmp(p->data, p->next->data) <= 0) {
 		p = p->next;
 	}
-	
+
 	return p;
 }
 
