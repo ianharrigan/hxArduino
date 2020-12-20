3e5646ef4f9c9a52ef2a9c5a3acf53eb680438b4
Yuuki Wesp <ls-micro@ya.ru>
2020-12-20 16:40:02 +0300

    use printing serial data without new line

--- src/ast2obj/builders/Monitor.hx
+++ src/ast2obj/builders/Monitor.hx
@@ -50,8 +50,9 @@ class Monitor {
 
         while (true) {
             var i = serialObj.available();
-            var s = serialObj.readBytes(i);
-            neko.Lib.print(s);
+            if (i != 0) {
+                Sys.print(serialObj.readBytes(i));
+            }
             Sys.sleep(0.01);
         }
 
